472931e7e2df7e3e9666c484f6a2bc20385d04cc
mapkkkk <epyc7742@126.com>
2024-06-09 22:44:27 +0800

    trans cn

--- CalcLidarData.py
+++ CalcLidarData.py
@@ -1,7 +1,7 @@
 import math
 
 class LidarData:
-    #def __init__(self, FSA, LSA, CS, Speed, TimeStamp, Confidence_i, Angle_i, Distance_i):
+    # def __init__(self, FSA, LSA, CS, Speed, TimeStamp, Confidence_i, Angle_i, Distance_i):
     def __init__(self, FSA, LSA, CS, Speed, TimeStamp, Degree_angle, Angle_i, Distance_i):
         self.FSA = FSA
         self.LSA = LSA
@@ -10,30 +10,28 @@ class LidarData:
         self.TimeStamp = TimeStamp
         self.Degree_angle = Degree_angle
 
-        #self.Confidence_i = Confidence_i
+        # self.Confidence_i = Confidence_i
         self.Angle_i = Angle_i
         self.Distance_i = Distance_i
 
-
-
 def CalcLidarData(str):
-    # Loại bỏ dấu cách
-    str = str.replace(' ','')
-    # Speed 2 byte, vì khi đọc string bị đảo ngược nên phải cộng ngược lại
-        # Tốc độ quay Lidar, đơn vị (degree/s)
+    # 去掉空格
+    str = str.replace(' ', '')
+    # Speed 2字节，因为读取字符串时会颠倒顺序，所以需要反向拼接
+        # Lidar的旋转速度，单位（度/秒）
     Speed = int(str[2:4] + str[0:2], 16) / 100
 
-    # Start angle: góc đầu tiên của data packet
-        # Đơn vị (0.01 degree)
+    # Start angle: 数据包的起始角度
+        # 单位（0.01度）
     FSA = float(int(str[6:8] + str[4:6], 16)) / 100
 
-    # End angle: góc cuối của data packet
+    # End angle: 数据包的结束角度
     LSA = float(int(str[-8:-6] + str[-10:-8], 16)) / 100
 
-    # Thời gian capture của packet, đơn vị (ms)
+    # 数据包的捕获时间，单位（毫秒）
     TimeStamp = int(str[-4:-2] + str[-6:-4], 16)
 
-    # CRC check
+    # CRC 校验
     CS = int(str[-2:], 16)
 
     Confidence_i = list()
@@ -41,8 +39,8 @@ def CalcLidarData(str):
     Distance_i = list()
     Degree_angle = list()
 
-    # Tính giá trị góc của packet
-        # Chia 12 tức khoảng đo của 12 điểm trong 1 packet
+    # 计算数据包的角度值
+        # 除以12表示一个数据包中12个点的间隔
     if(LSA - FSA > 0):
         angleStep = float(LSA - FSA) / 12
     else:
@@ -50,18 +48,17 @@ def CalcLidarData(str):
 
     counter = 0
     circle = lambda deg : deg - 360 if deg >= 360 else deg
-    # Phân tích 12 điểm trong 1 packet
+    # 分析一个数据包中的12个点
     for i in range(0, 6 * 12, 6):
-        # Khoảng cách, đơn vị (mm)
+        # 距离，单位（毫米）
         Distance_i.append(int(str[8+i+2 : 8+i+4] + str[8+i : 8+i+2], 16) / 1000)
-        # Intensity, cường độ ánh sáng, càng lớn tức độ tin cậy càng chính xác
-        #Confidence_i.append(int(str[8+i+4 : 8+i+6], 16))
-        # Góc chiếu của điểm trong packet
+        # Intensity，光强度，越大表示可信度越高
+        # Confidence_i.append(int(str[8+i+4 : 8+i+6], 16))
+        # 数据包中点的角度
         Degree_angle.append(circle(angleStep * counter + FSA))
         Angle_i.append(circle(angleStep * counter + FSA) * math.pi / 180.0)
         counter += 1
 
     lidarData = LidarData(FSA, LSA, CS, Speed, TimeStamp, Degree_angle, Angle_i, Distance_i)
-    # Return data của Lidar
+    # 返回Lidar的数据
     return lidarData
-
